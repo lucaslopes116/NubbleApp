607472bff943a55364c836770163e773ff279d90
Lucas Lopes <lucaslopes116@hotmail.com>
2023-06-09 19:13:20 -0300

    add theme e components box

--- App.tsx
+++ App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 import {SafeAreaView} from 'react-native';
 import {Text} from './src/components/Text/Text';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text preset="headingLarge" italic>
-        Coffstack
-      </Text>
-      <Text preset="headingMedium">Coffstack</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge" italic>
+          Coffstack
+        </Text>
+        <Text preset="headingMedium">Coffstack</Text>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
