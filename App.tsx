f17fa509536c96655f414c90f676569215ecc0d2
Lucas Lopes <lucaslopes116@hotmail.com>
2023-06-20 11:20:08 -0300

    create component textInput

--- App.tsx
+++ App.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
 
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+import {Box} from './src/components/Box/Box';
+import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <Text preset="headingLarge" italic>
-          Coffstack
-        </Text>
-        <Text preset="headingMedium">Coffstack</Text>
+        <View style={{paddingHorizontal: 24}}>
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+
+          <Box mb="s20">
+            <TextInput label="E-mail" />
+          </Box>
+          <Box>
+            <TextInput label="Senha" />
+          </Box>
+
+          <Text color="primary" preset="paragraphSmall" bold mt="s10">
+            Esqueci minha senha
+          </Text>
+
+          <Button marginTop="s48" title="Entrar" />
+          <Button preset="outline" marginTop="s12" title="Criar uma conta" />
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   );
