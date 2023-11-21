151dbd96a98f07fecaf492ad03d2bd19a9388e18
Lucas Lopes <lucaslopes116@hotmail.com>
2023-11-21 18:46:13 -0300

    add faketimeouts

--- src/screens/app/SearchScreen/__tests__/integration/SearchScreen.test.tsx
+++ src/screens/app/SearchScreen/__tests__/integration/SearchScreen.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {authCredentialsStorage} from '@services';
 import {mockUtils, server, userMocked} from '@test';
-import {fireEvent, renderScreen, screen} from 'test-utils';
+import {act, fireEvent, renderScreen, screen} from 'test-utils';
 
 import {AppStack} from '@routes';
 
@@ -10,6 +10,7 @@ jest.unmock('@react-navigation/native');
 
 beforeAll(() => {
   server.listen();
+  jest.useFakeTimers();
   jest
     .spyOn(authCredentialsStorage, 'get')
     .mockResolvedValue(mockUtils.mateusAuthCredentials);
@@ -21,25 +22,31 @@ afterEach(() => {
 
 afterAll(() => {
   server.close();
+  jest.useRealTimers();
   jest.resetAllMocks();
 });
 
 describe('integration: SearchScreen', () => {
   test('Search Flow', async () => {
+    // 1) Navigate to Search Screen
     renderScreen(<AppStack initialRouteName="SearchScreen" />);
 
+    // 2) Find the search input and type user name
     const inputText = screen.getByPlaceholderText(/digite sua busca/i);
-
     fireEvent.changeText(inputText, 'mar');
+    act(() => jest.runAllTimers());
 
+    // 3) Find two users as per the MSW mock
     const user1 = await screen.findByText(userMocked.user1.username);
     expect(user1).toBeTruthy();
 
     const user2 = await screen.findByText(userMocked.user2.username);
     expect(user2).toBeTruthy();
 
+    // 4) Select the user1 and navigate to Profile Screen
     fireEvent.press(user1);
 
+    // 5) Expect to be at the Profile Screen with the user1 loaded
     const userFullName = await screen.findByText(userMocked.user1.full_name);
     expect(userFullName).toBeTruthy();
   });
