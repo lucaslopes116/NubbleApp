db6f23eac70bcb44ad5b9d78f38d9b7a1afe8586
Lucas Lopes <lucaslopes116@hotmail.com>
2023-11-21 18:33:08 -0300

    test integration searchScreen find user in screen

--- src/domain/User/index.ts
+++ src/domain/User/index.ts
@@ -3,3 +3,4 @@ export * from './userService';
 export * from './useCases/useUserGetById';
 export * from './userAdapter';
 export * from './useCases/useUserSearch';
+export {USER_PATH} from './userApi';
