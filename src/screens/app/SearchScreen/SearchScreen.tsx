3daf0648d93a678fb46d71c6582c5ebefc7589db
Lucas Lopes <lucaslopes116@hotmail.com>
2023-11-21 14:58:15 -0300

    function getlist

--- src/screens/app/SearchScreen/SearchScreen.tsx
+++ src/screens/app/SearchScreen/SearchScreen.tsx
@@ -1,10 +1,16 @@
 import React, {useState} from 'react';
 
+import {useUserSearch} from '@domain';
+
 import {Icon, Screen, Text, TextInput} from '@components';
+import {useDebounce} from '@hooks';
 import {AppScreenProps} from '@routes';
 
 export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
   const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search);
+
+  const {list} = useUserSearch(debouncedSearch);
   return (
     <Screen
       canGoBack
@@ -15,7 +21,9 @@ export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
           LeftComponent={<Icon color="gray3" name="search" />}
         />
       }>
-      <Text>Search Screen</Text>
+      {list.map(user => (
+        <Text key={user.id}>{user.username}</Text>
+      ))}
     </Screen>
   );
 }
