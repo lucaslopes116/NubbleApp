b2cae19e03e770fd86517cf2a96eacc022ed523f
Lucas Lopes <lucaslopes116@hotmail.com>
2023-11-21 14:53:10 -0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,12 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            endOfLine: 'auto',
+          },
+        ],
         'import/order': [
           'error',
           {
