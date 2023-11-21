c2d08f5644d861831ae72440a4f034b11fdf76e4
Lucas Lopes <lucaslopes116@hotmail.com>
2023-11-21 18:49:09 -0300

    finished testing search

--- src/screens/app/SearchScreen/__tests__/integration/SearchScreen.test.tsx
+++ src/screens/app/SearchScreen/__tests__/integration/SearchScreen.test.tsx
@@ -49,5 +49,34 @@ describe('integration: SearchScreen', () => {
     // 5) Expect to be at the Profile Screen with the user1 loaded
     const userFullName = await screen.findByText(userMocked.user1.full_name);
     expect(userFullName).toBeTruthy();
+
+    // 6) Press the back button to navigate back to Search Screen
+    const backButton = screen.getByTestId('screen-back-button');
+    fireEvent.press(backButton);
+
+    // 7) Clean the search input
+    const inputTextAfterBack = screen.getByPlaceholderText(/digite sua busca/i);
+    fireEvent.changeText(inputTextAfterBack, '');
+    act(() => jest.runAllTimers());
+
+    // 8) Make sure the search history (buscas recentes) section appears
+    const searchHistoryTitle = screen.getByText(/buscas recentes/i);
+    expect(searchHistoryTitle).toBeTruthy();
+
+    // 9) The user1 (pressed) was the saved in the search history
+    const user1AfterBack = screen.queryByText(userMocked.user1.username);
+    expect(user1AfterBack).toBeTruthy();
+
+    // 10) The user2 (NOT pressed) must NOT appear in the search history
+    const user2AfterBack = screen.queryByText(userMocked.user2.username);
+    expect(user2AfterBack).toBeFalsy();
+
+    // 11) Remove user1 from the search history by pressing the trash icon
+    const trashIcon = screen.getByTestId('trash');
+    fireEvent.press(trashIcon);
+
+    // 12) Make sure the user1 was removed from the search history
+    const user1AfterRemoved = screen.queryByText(userMocked.user1.username);
+    expect(user1AfterRemoved).toBeFalsy();
   });
 });
