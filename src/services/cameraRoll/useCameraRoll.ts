510350748301125259745b0ce808f07054da87f6
Lucas Lopes <lucaslopes116@hotmail.com>
2024-01-18 13:54:52 -0300

    Publicar Post Screen

--- src/services/cameraRoll/useCameraRoll.ts
+++ src/services/cameraRoll/useCameraRoll.ts
@@ -5,7 +5,10 @@ import {useInfiniteQuery} from '@tanstack/react-query';
 
 import {cameraRollService} from './cameraRollService';
 
-export function useCameraRoll(hasPermission: boolean) {
+export function useCameraRoll(
+  hasPermission: boolean,
+  onInitialLoad?: (imageUri: string) => void,
+) {
   const [list, setList] = useState<string[]>([]);
 
   const query = useInfiniteQuery({
@@ -21,8 +24,12 @@ export function useCameraRoll(hasPermission: boolean) {
         return [...prev, ...curr.photoList];
       }, []);
       setList(newList);
+
+      if (query.data.pages.length === 1 && onInitialLoad) {
+        onInitialLoad(newList[0]);
+      }
     }
-  }, [query.data]);
+  }, [onInitialLoad, query.data]);
 
   return {
     photoList: list,
