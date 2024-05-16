d3d85debda5e44c266925aee1a1e3d09ebde42c4
Lucas Lopes <lucaslopes116@hotmail.com>
2024-05-16 18:20:27 -0300

    splash screen, icons, orientation screen

--- android/app/src/main/java/com/nubbleapp/MainActivity.java
+++ android/app/src/main/java/com/nubbleapp/MainActivity.java
@@ -5,14 +5,16 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
+
 import android.os.Bundle;
 public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    RNBootSplash.init(this, R.style.BootTheme);
+    super.onCreate(savedInstanceState);
   }
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
